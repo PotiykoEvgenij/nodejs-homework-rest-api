720378d3e51da0137e484a07dbdc8d9172a323b0
PotiykoEvgenij <potijkoevgenij@gmail.com>
2023-08-30 21:33:34 +0300

    Update contacts.js

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const router = express.Router();
 
-const contactsController = require('../../contorllers/contactsController');
+const contactsController = require('../../contorllers/contacts');
 
 // const {
 //   listContacts,
