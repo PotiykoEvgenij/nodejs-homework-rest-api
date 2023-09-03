a455c0f7489d6cfbcebf082bcf6b17bc83b930be
PotiykoEvgenij <potijkoevgenij@gmail.com>
2023-09-03 17:45:37 +0300

    hw4

--- contorllers/auth/register.js
+++ contorllers/auth/register.js
@@ -1,7 +1,25 @@
+const bcrypt = require('bcrypt');
+
 const User = require('../../models/user');
 
-function register(req, res, next) {
-    res.send('Register');
-}
+async function register(req, res, next) {
+    const { password, email, subscription } = req.body;
+
+    try {
+        const user = await User.findOne({ email }).exec();
+
+        if (user !== null) {
+            return res.status(409).send({ message: "User already exist" });
+        };
+
+        const passwordHash = await bcrypt.hash(password, 10);
+
+        await User.create({ password: passwordHash, email, subscription });
+
+        res.status(201).send({ message: "Registration successful" });
+    } catch (error) {
+        next(error);
+    }
+};
 
 module.exports = register;
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,9 +1,10 @@
 const express = require('express');
 
-const AuthController = require('../../controllers/auth');
+const AuthController = require('../../contorllers/auth');
 
 const router = express.Router();
+const jsonParses = express.json();
 
-router.post('/register', AuthController.register);
+router.post('/register', jsonParses, AuthController.register);
 
 module.exports = router;
